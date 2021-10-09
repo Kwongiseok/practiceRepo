ee461780870d542dc87ea02c1fa40f44dc5deff9
ri2377@naver.com <ri2377@naver.com>
2021-10-10 01:11:17 +0900

    NextJS에서 Link 동작 확인

---
+++ nextJS/firstproject/pages/blog.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Blog = (props) => (
+  <div>
+    <h1>Blog</h1>
+  </div>
+);
+
+export default Blog;
---
+++ nextJS/firstproject/pages/hey/ho.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ho = (props) => (
+  <div>
+    <h1>ho!!</h1>
+  </div>
+);
+
+export default ho;

2476c5f49114a8de900a74b539aa6c747c7642d0
ri2377@naver.com <ri2377@naver.com>
2021-10-10 01:33:06 +0900

    prefetch 개념 확인

--- nextJS/firstproject/pages/blog.js
+++ nextJS/firstproject/pages/blog.js
@@ -1,8 +1,20 @@
-import React from "react";
+import Link from "next/link";
+import posts from "../posts.json";
 
-const Blog = (props) => (
+const Blog = () => (
   <div>
     <h1>Blog</h1>
+    <ul>
+      {Object.entries(posts).map((value, index) => {
+        return (
+          <li key={index}>
+            <Link href="/blog/[id]" prefetch={false} as={"/blog/" + value[0]}>
+              <a>{value[1].title}</a>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   </div>
 );
 
--- nextJS/firstproject/pages/hey/ho.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ho = (props) => (
-  <div>
-    <h1>ho!!</h1>
-  </div>
-);
-
-export default ho;

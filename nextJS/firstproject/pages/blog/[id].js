5c469b289b1700df78c18339d03f7e1cd3d08d93
ri2377@naver.com <ri2377@naver.com>
2021-10-10 13:32:23 +0900

    번들 JS에서 임포트하지 않고 따로 번들로 분리하는 방법

--- nextJS/firstproject/pages/blog/[id].js
+++ nextJS/firstproject/pages/blog/[id].js
@@ -4,14 +4,18 @@ const Post = (props) => {
   return (
     <div>
       <h1>{props.post.title}</h1>
+      <p>Published on {props.date}</p>
       <p>{props.post.content}</p>
     </div>
   );
 };
 
-Post.getInitialProps = ({ query }) => {
+Post.getInitialProps = async ({ query }) => {
+  // moment 모듈을 컴포넌트단에서 import 해오면 용량이 크다. => 따로 번들로 분리하는 방법
+  const moment = (await import("moment")).default();
   return {
     post: posts[query.id],
+    date: moment.format("dddd D MMMM YYYY"),
   };
 };
 

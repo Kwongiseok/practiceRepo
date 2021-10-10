bc84f2ae3d01d6bb59cc2f495837ebdf66b5ee87
ri2377@naver.com <ri2377@naver.com>
2021-10-10 21:10:49 +0900

    dynamic routes

--- nextJS/nextjs-blog/pages/posts/[id].js
+++ nextJS/nextjs-blog/pages/posts/[id].js
@@ -1,15 +1,22 @@
 import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/Date";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
+      <Head>{postData.title}</Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <br />
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -23,7 +30,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,

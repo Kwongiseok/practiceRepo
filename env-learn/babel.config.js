c49170ed29bccb13f1fcc5b521259ef84cd6db9c
ri2377@naver.com <ri2377@naver.com>
2021-10-12 23:47:09 +0900

    폴리필, 코어 js

--- env-learn/babel.config.js
+++ env-learn/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ["./my-babel-preset.js"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3,
+        },
+      },
+    ],
+  ],
+  targets: {
+    chrome: "79",
+    ie: "11",
+  },
 };

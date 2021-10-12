9220448df9dd4c91f100e20e9c42ef19d259a712
ri2377@naver.com <ri2377@naver.com>
2021-10-12 14:10:32 +0900

    css-loader

--- env-learn/webpack.config.js
+++ env-learn/webpack.config.js
@@ -7,6 +7,15 @@ module.exports = {
   },
   output: {
     path: path.resolve("./dist"),
-    filename: "[name].js",
+    filename: "[name].js", // output name을 동적으로 (entry의 이름으로 결정이 된다.) 생성할 때 사용
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+    ],
   },
 };
